606674ccbc1b43acbe711d768c6af8abc18f93c6
JowayYoung <young.joway@aliyun.com>
2021-10-14 20:39:03 +0800

    增加TabBar

--- src/app.config.js
+++ src/app.config.js
@@ -5,6 +5,31 @@ export default {
 		"pages/plan/index",
 		"pages/mine/index"
 	],
+	tabBar: {
+		color: "#333",
+		list: [{
+			iconPath: "assets/icon/stats.png",
+			pagePath: "pages/stats/index",
+			selectedIconPath: "assets/icon/stats-active.png",
+			text: "统计"
+		}, {
+			iconPath: "assets/icon/chart.png",
+			pagePath: "pages/chart/index",
+			selectedIconPath: "assets/icon/chart-active.png",
+			text: "图表"
+		}, {
+			iconPath: "assets/icon/plan.png",
+			pagePath: "pages/plan/index",
+			selectedIconPath: "assets/icon/plan-active.png",
+			text: "计划"
+		}, {
+			iconPath: "assets/icon/mine.png",
+			pagePath: "pages/mine/index",
+			selectedIconPath: "assets/icon/mine-active.png",
+			text: "我的"
+		}],
+		selectedColor: "#3c9"
+	},
 	window: {
 		backgroundTextStyle: "light", // 下拉加载样式
 		navigationBarBackgroundColor: "#fff", // 导航背景颜色
