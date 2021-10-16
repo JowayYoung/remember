98d25a9855195fdd135748149936d8dbc0138694
JowayYoung <young.joway@aliyun.com>
2021-10-16 22:21:53 +0800

    完成明细页面的静态结构

---
+++ src/mocks/stats-list.js
@@ -0,0 +1,69 @@
+const STATS_LIST = [{
+	date: "2021-10-15",
+	list: [{
+		money: -7963.66,
+		bigType: "车房还款",
+		smallType: "住宅"
+	}, {
+		money: -1665.31,
+		bigType: "车房还款",
+		smallType: "车位"
+	}, {
+		money: -2697.53,
+		bigType: "车房还款",
+		smallType: "汽车"
+	}]
+}, {
+	date: "2021-10-14",
+	list: [{
+		money: 350,
+		bigType: "广告流量",
+		smallType: "广告"
+	}]
+}, {
+	date: "2021-10-13",
+	list: [{
+		money: -7963.66,
+		bigType: "车房还款",
+		smallType: "住宅"
+	}, {
+		money: -1665.31,
+		bigType: "车房还款",
+		smallType: "车位"
+	}, {
+		money: -2697.53,
+		bigType: "车房还款",
+		smallType: "汽车"
+	}]
+}, {
+	date: "2021-10-12",
+	list: [{
+		money: 350,
+		bigType: "广告流量",
+		smallType: "广告"
+	}]
+}, {
+	date: "2021-10-11",
+	list: [{
+		money: -7963.66,
+		bigType: "车房还款",
+		smallType: "住宅"
+	}, {
+		money: -1665.31,
+		bigType: "车房还款",
+		smallType: "车位"
+	}, {
+		money: -2697.53,
+		bigType: "车房还款",
+		smallType: "汽车"
+	}]
+}, {
+	date: "2021-10-10",
+	list: [{
+		money: 350,
+		bigType: "广告流量",
+		smallType: "广告"
+	}]
+}];
+
+export default STATS_LIST;
--- src/pages/stats/index.jsx
+++ src/pages/stats/index.jsx
@@ -1,40 +1,56 @@
 import React, { useState } from "react"; // eslint-disable-line no-unused-vars
 import { Picker, Text, View } from "@tarojs/components";
 import Day from "dayjs";
-import { AtFab, AtActionSheet, AtActionSheetItem } from "taro-ui";
 
 import "./index.scss";
+import STATS_LIST from "../../mocks/stats-list";
+import { DAYS } from "../../utils/getting";
 
 export default function Stats() {
 	const [date, setDate] = useState(Day().format("YYYY-MM"));
-	const [visible, setVisible] = useState(false);
 	const onChangeDate = e => setDate(e.detail.value);
-	const onShowModal = () => setVisible(true);
-	const onHideModal = () => setVisible(false);
+	const statsDom = STATS_LIST.map(v => {
+		const date = Day(v.date);
+		const amount = v.list.reduce((t, w) => t + w.money, 0).toFixed(2);
+		return (
+			<View className="stats-detail-item">
+				<View className="stats-detail-title">
+					<Text>{date.format("MM-DD")} 星期{DAYS[date.day()]}</Text>
+					<Text>{amount >= 0 ? "收入" : "支出"}：{amount}</Text>
+				</View>
+				<View className="stats-detail-list">
+					{v.list.map(w => (
+						<View className="stats-detail-data">
+							<View className="stats-detail-type">
+								<View className="stats-detail-icon one" />
+								<Text className="stats-detail-mark">{w.bigType}-{w.smallType}</Text>
+							</View>
+							<Text className="stats-detail-money">{w.money}</Text>
+						</View>
+					))}
+				</View>
+			</View>
+		);
+	});
 	return (
 		<View className="stats-page">
 			<View className="stats-summary">
 				<View className="stats-summary-title">
-					<View className="stats-summary-title-item">日期</View>
-					<View className="stats-summary-title-item">收入</View>
-					<View className="stats-summary-title-item">支出</View>
+					<Text className="stats-summary-title-item">日期</Text>
+					<Text className="stats-summary-title-item">收入</Text>
+					<Text className="stats-summary-title-item">支出</Text>
 				</View>
 				<View className="stats-summary-body">
 					<Picker className="stats-summary-body-item" mode="date" fields="month" onChange={onChangeDate}>
 						<Text className="stats-summary-body-date">{date}</Text>
 					</Picker>
-					<View className="stats-summary-body-item">99999.99</View>
-					<View className="stats-summary-body-item">99999</View>
+					<Text className="stats-summary-body-item">99999.99</Text>
+					<Text className="stats-summary-body-item">99999</Text>
 				</View>
 			</View>
-			<View className="stats-detail"></View>
-			<AtFab className="stats-btn" size="small" onClick={onShowModal}>
-				<Text className="at-fab__icon at-icon at-icon-add"></Text>
-			</AtFab>
-			<AtActionSheet title="请选择记账类型" cancelText="取消" isOpened={visible} onCancel={onHideModal}>
-				<AtActionSheetItem>收入</AtActionSheetItem>
-				<AtActionSheetItem>支出</AtActionSheetItem>
-			</AtActionSheet>
+			<View className="stats-detail">
+				<View className="stats-detail-wrapper">{statsDom}</View>
+			</View>
 		</View>
 	);
 }
