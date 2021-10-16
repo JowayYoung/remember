bf9759acdf38635389957a0a0384e2b74afaa949
JowayYoung <young.joway@aliyun.com>
2021-10-16 22:43:42 +0800

    完成图表页面的静态结构

--- src/pages/chart/index.jsx
+++ src/pages/chart/index.jsx
@@ -1,10 +1,15 @@
 import React from "react"; // eslint-disable-line no-unused-vars
-import { Text, View } from "@tarojs/components";
+import { Image, Text, View } from "@tarojs/components";
+
+import "./index.scss";
+import ImgComing from "../../assets/img/coming.png";
 
 export default function Chart() {
 	return (
 		<View className="chart-page">
-			<Text>图表</Text>
+			<Image className="chart-placeholder" src={ImgComing} />
+			<Text>图表功能有助于分析你的消费观念</Text>
+			<Text>正在努力开发中，敬请期待！！！</Text>
 		</View>
 	);
 }
